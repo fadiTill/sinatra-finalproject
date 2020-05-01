0a33b481fc227bed3a27fdb9ebe4bb15e24f3281
github username <github email address>
2020-05-01 15:09:35 -0700

    create guest table and columns

--- db/migrate/20200501205951_create_guests.rb
+++ db/migrate/20200501205951_create_guests.rb
@@ -1,4 +1,15 @@
 class CreateGuests < ActiveRecord::Migration
   def change
+    create_table :guests do |t|
+      t.string  :name
+      t.integer :phone_number
+      t.string  :adresse
+      t.string  :email
+      t.string  :time_line 
+      t.text    :note
+  
+        t.timestamps
+    end
   end
-end
+ end
+  
