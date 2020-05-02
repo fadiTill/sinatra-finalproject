699ad9af94fb210bafc64afe5488b3fb9053567d
github username <github email address>
2020-05-01 17:37:59 -0700

    view new/views views/ welcome update

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,9 +1,38 @@
 class GuestsController < ApplicationController
 
-    get "/guests/id" do
-        erb :"guests/index"
-end
+    
     get "/guests/index" do
         erb :"guests/index"
 end
-end
+
+get "/guests/new" do
+    erb :"guests/new"
+    
+    
+ end
+
+#  post"guests/" 
+# end
+ 
+# get "/guests/:id" do
+    
+#     erb :"guests/show"
+#  end
+
+#  get "/guests/:id/edit" do
+    
+    
+#  end
+
+#  patch "/"guests/:id
+# end
+
+
+# # get "/guests/:id/delete" do
+# # end
+
+
+# delete "/guests/:id/delete"
+# end
+
+ end
