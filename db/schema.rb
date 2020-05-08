d39b5799f7d489efbb5bd332d63ca20d066e471f
github username <github email address>
2020-05-08 09:57:22 -0700

    migrate Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200501205951) do
+ActiveRecord::Schema.define(version: 20200508164530) do
 
   create_table "guests", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20200501205951) do
     t.text     "note"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
