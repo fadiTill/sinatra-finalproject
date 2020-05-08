cfc507fc9d060eb2590f054e4ea290c331c5bec4
github username <github email address>
2020-05-08 10:41:57 -0700

    created  MVS users/sessions files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+
+
+    def "/users/:id" do
+
+end
+
+get "/users/sign_up" do
+end 
+
+
+post "/users/sign_up" do
+end
+
+
+delete "/logout" do 
+end 
+
+
+delete "/users/deactivate" do 
+end
